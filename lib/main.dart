d6911cdf231a3f0dd10888ec4ea6868bc27d1c48
U-T-kuroitigo <git.cq.kx@gmail.com>
2023-07-03 12:29:17 +0900

    使用可能状態まで確認

--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,72 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dart_openai/dart_openai.dart';
 
-//テストコメント
-
 Future<void> main() async{
-  // main関数内で非同期処理を呼び出すための設定
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  // デバイスで使用可能なカメラのリストを取得
-  final cameras = await availableCameras();
-
-  // 利用可能なカメラのリストから特定のカメラを取得
-  final firstCamera = cameras.first;
-
-  // 取得できているか確認
-  print(firstCamera);
-
-
-
-  // .evnから環境変数を読み込む
+  // // .evnから環境変数を読み込む
   await dotenv.load(fileName: '.env');
   OpenAI.apiKey = dotenv.get('OPEN_AI_API_KEY');
+
   //使用してみる
   final completion = await OpenAI.instance.chat.create(
+    //モデルはこれで
     model: "gpt-3.5-turbo",
+    //以下が中身の処理
     messages: [
       OpenAIChatCompletionChoiceMessageModel(
-        content: "hello, what is Flutter and Dart ?",
+        //ここに質問を書く
+        content: "豚肉を使ったレシピ",
+        //ここに機能を書くuserは質問に関する回答
         role: OpenAIChatMessageRole.user,
       ),
     ],
   );
+  OpenAIChatCompletionChoiceMessageModel resultM = completion.choices.first.message;
+
+
+
+  //返答を表示
   print(completion.choices.first.message);
 
-  OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
-    model: "gpt-3.5-turbo",
-    messages: [
-      OpenAIChatCompletionChoiceMessageModel(
-        content: "hello, what is Flutter and Dart ?",
-        role: OpenAIChatMessageRole.user,
-      ),
-    ],
-  );
-  print(chatCompletion.choices.first.message);
+  //
+  // OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
+  //   model: "gpt-3.5-turbo",
+  //   messages: [
+  //     OpenAIChatCompletionChoiceMessageModel(
+  //       content: "hello, what is Flutter and Dart ?",
+  //       role: OpenAIChatMessageRole.user,
+  //     ),
+  //   ],
+  // );
+  // print(chatCompletion.choices.first.message);
 
 
 
 
-  runApp(MyApp(camera: firstCamera));
+  runApp(MyApp());
 }
 
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    Key? key,
-    required this.camera,
-  }):super(key: key);
 
-
-  final CameraDescription camera;
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Camera Prc',
+      title: "ひややっ庫",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.cyan,
       ),
-      home: TakePictureScreen(camera: camera),
+      routes: {
+        '/': (context) => SizedBox(),
+      },
     );
   }
 }
 
-class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({Key? key,
-    required this.camera,
-  }): super(key: key);
-
-
-
-  final CameraDescription camera;
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState(){
-    super.initState();
-
-    _controller = CameraController(
-      // カメラを指定
-      widget.camera,
-      //　解像度を指定
-      ResolutionPreset.medium,
-    );
-    // コントローラーを初期化
-    _initializeControllerFuture = _controller.initialize().then((_){
-      setState(() {});
-    });
-  }
-
-  void dispose(){
-    // ウィジェットが破棄されたらコントローラーを破棄
-    _controller.dispose();
-    super.dispose();
-  }
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot){
-            if(snapshot.connectionState == ConnectionState.done){
-              return CameraPreview(_controller);
-            }else{
-              return const CircularProgressIndicator();
-            }
-          },
-            ),
-        ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // 写真を撮る
-          final image = await _controller.takePicture();
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DisplayPictureScreen(imagePath: image.path),
-              fullscreenDialog: true,
-            ),
-          );
-    },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
-
-// 撮影した写真を表示する画面
-class DisplayPictureScreen extends StatelessWidget{
-  const DisplayPictureScreen({
-    Key? key,
-    required this.imagePath
-  }): super(key: key);
-
-  final String imagePath;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('撮れた写真')),
-      body: Center(child: Image.file(File(imagePath))),
-    );
-  }
-}
