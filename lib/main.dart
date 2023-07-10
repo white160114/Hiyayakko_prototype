55cb664eb6142d0824d71787fe01b1af6c2a1409
2220394 <2220394@ecc.ac.jp>
2023-07-10 23:30:05 +0900

    add:右上のdebugラベル削除

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "ひややっ庫",
       theme: ThemeData(
         primaryColor: Colors.cyan,
