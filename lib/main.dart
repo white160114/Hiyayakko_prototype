373a25aafd176bfe897753c16843eaa31226259c
2220394 <2220394@ecc.ac.jp>
2023-07-15 23:55:42 +0900

    add:mainにルーティング追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hiyayakko/Controllers/getcode_controller.dart';
 import 'package:hiyayakko/Controllers/test1_controller.dart';
+import 'package:hiyayakko/Views/editpro_page.dart';
+import 'package:hiyayakko/Views/profiel_page.dart';
 
+import 'Controllers/editpro_controller.dart';
+import 'Controllers/profiel_controller.dart';
 import 'Controllers/test2_controller.dart';
 
 void main() {
@@ -16,13 +21,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.cyan,
       ),
-      initialRoute: '/test',
+      initialRoute: '/profiel',
       routes: {
         //ルーディング例
         //url: (context) => ページクラス
         '/': (context) => SizedBox(),
         '/test': (context) => Test1Controller(),
         '/test2page': (context) => Test2Controller(),
+        '/profiel': (context) => ProfielController(),
+        '/edit': (context) => EditproController(),
+        '/getcode':(context) => GetCodeController(),
       },
     );
   }
