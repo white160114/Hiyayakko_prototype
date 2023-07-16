a009dfe97d7897ec6fb298f0dd71f10e431fe6e6
2220394 <2220394@ecc.ac.jp>
2023-07-16 14:06:48 +0900

    add:カテゴリ―コントローラの追加

---
+++ lib/Controllers/ctegory_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hiyayakko/Views/category_page.dart';
+
+class CategoryController  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return CategoryPage();
+  }
+
+}
