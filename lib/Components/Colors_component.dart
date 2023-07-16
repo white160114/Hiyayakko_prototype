155104dd3e5171155bced71fd5574696fd885fe8
2220394 <2220394@ecc.ac.jp>
2023-07-16 14:06:20 +0900

    add:コンポーネント追加

---
+++ lib/Components/Colors_component.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Colors_component {
+  static const globalBackgroundColor = Color(0xFFEAEAEA);
+  static const titleColor = Color(0xFF928E65);
+  static const gold = Color(0xFF9B8124);
+
+}
