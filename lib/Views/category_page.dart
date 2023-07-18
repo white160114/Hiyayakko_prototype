d8ad5f87d7f94919aa11a139eee76584255e4cbe
2220394 <2220394@ecc.ac.jp>
2023-07-18 15:03:55 +0900

    fix:カテゴリーが表示されるように表示

--- lib/Views/category_page.dart
+++ lib/Views/category_page.dart
@@ -10,42 +10,198 @@ class CategoryPage extends StatelessWidget{
     return Scaffold(
      body: Column(
        children:[
-         Row(
-           children:[
-             CustomBackButton(
-               onPressed: (){
-
-               },
-             ),
-             SizedBox(width: 30,),
-             Text(
-               'カテゴリー',
-               style: TextStyle(
-                 color: Colors_component.titleColor,
-                 fontSize: 20,
+         Container(
+           child: Row(
+             children: [
+               Padding(
+                   padding: EdgeInsets.only(left: 30, top: 40, right: 10),
+                   child: CustomBackButton(
+                     onPressed: () {
+                       Navigator.pop(context); // 1ページ戻る処理
+                     },
+                   )
                ),
-             ),
-             ElevatedButton(
+               SizedBox(width: 10),
+               Padding(
+                 padding: EdgeInsets.only(top: 40),
+                 child: Text(
+                   'カテゴリー',
+                   style: TextStyle(
+                     fontSize: 25,
+                     // fontWeight: FontWeight.bold,
+                     color: Colors_component.titleColor,
+                   ),
+                 ),
+               ),
+               SizedBox(width: 10,),
+               Padding(
+                 padding: EdgeInsets.only(top: 40),
+                 child:CustomAddButton(
+                   onPressed: (){
+                      Navigator.pushNamed(context, '/addcategory',);
+                   },
+                 )
+               ),
+             ],
+           ),
+         ),
+         SizedBox(height: 100,),
+         Container(
+           alignment: Alignment.center,
+           child: Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               CustomButton(
+                 onPressed: (){
+                    Navigator.pushNamed(context, "/meat");
+                 },
+                 imagePath: 'lib/Views/Images/meat.png',
+                 label: '肉',
+                 textSize: 15,
+               ),
+               SizedBox(width: 5),
+               CustomButton(
                  onPressed: (){
 
                  },
-                 style: ElevatedButton.styleFrom(
-                   shape: CircleBorder(),
-                   backgroundColor: Color.fromARGB(255, 164, 237, 92),
-                   fixedSize: Size(50, 50), // ボタンの幅と高さを指定
-                   elevation: 10, // 影の大きさを指定
-                 ),
-                 child: Center(
-                   child: Icon(
-                     Icons.keyboard_return,
-                     size: 33, // アイコンのサイズを指定
-                   ),
-                 ),
-             ),
-          ]
+                 imagePath: 'lib/Views/Images/drink.png',
+                 label: '飲み物',
+               ),
+               SizedBox(width: 5),
+               CustomButton(
+                 onPressed: (){
+
+                 },
+                 imagePath: 'lib/Views/Images/salt.png',
+                 label: '調味料',
+               )
+             ],
+           ),
+         ),
+         SizedBox(height: 20,),
+         Container(
+           alignment: Alignment.center,
+           child: Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               CustomButton(
+                 onPressed: (){
+
+                 },
+                 imagePath: 'lib/Views/Images/fish.png',
+                 label: '魚',
+                 textSize: 15,
+               ),
+               SizedBox(width: 5),
+               CustomButton(
+                 onPressed: (){
+
+                 },
+                 imagePath: 'lib/Views/Images/beer.png',
+                 label: 'お酒',
+               ),
+               SizedBox(width: 5),
+               CustomButton(
+                 onPressed: (){
+
+                 },
+                 imagePath: null,
+                 label: 'その他',
+                 textSize: 15,
+               )
+             ],
+           ),
+         ),
+         SizedBox(height: 20,),
+         Container(
+           child: Row(
+             children: [
+               SizedBox(width: 12,),
+               CustomButton(
+                 onPressed: (){
+
+                 },
+                 imagePath: 'lib/Views/Images/vegetable.png',
+                 label: '野菜',
+                 textSize: 15,
+               ),
+             ],
+           ),
+         ),
+         SizedBox(height: 230,),
+         CustomNavbar(
+             colorFilters: [
+                ColorFilter.mode(Colors.white, BlendMode.modulate),
+                ColorFilter.mode(Colors.white, BlendMode.modulate),
+                ColorFilter.mode(Colors.white, BlendMode.modulate),
+                ColorFilter.mode(Colors.brown, BlendMode.modulate),
+                ColorFilter.mode(Colors.white, BlendMode.modulate),
+             ],
+           onTapCallbacks: [
+                 () {
+               // 1番目の画像がタップされたときの処理
+             },
+                 () {
+               // 2番目の画像がタップされたときの処理
+             },
+                 () {
+               // 3番目の画像がタップされたときの処理
+             },
+                 () {
+               // 4番目の画像がタップされたときの処理
+             },
+                 () {
+               // 5番目の画像がタップされたときの処理
+             },
+           ],
          )
        ]
      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String? imagePath;
+  final String label;
+  final double textSize;
+
+  const CustomButton({
+    required this.onPressed,
+    this.imagePath,
+    required this.label,
+    this.textSize = 12.5,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (imagePath != null)
+            Image.asset(
+              imagePath!,
+            ),
+          SizedBox(width: 5),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: textSize,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFFE1E1E1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        fixedSize: Size(120, 70),
+      ),
     );
   }
 }
