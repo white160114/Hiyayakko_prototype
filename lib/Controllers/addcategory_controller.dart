871ea25d502465db1663493a352dc53b03863970
2220394 <2220394@ecc.ac.jp>
2023-07-18 15:02:26 +0900

    add:コントローラー追加

---
+++ lib/Controllers/addcategory_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+import '../Views/addcategory_page.dart';
+
+class AddCategoryController  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return AddCategoryPage();
+  }
+}
