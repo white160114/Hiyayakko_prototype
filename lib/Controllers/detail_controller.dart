80b8c77b2d0d7464c1f26ce26ed78cd630c9ea7d
2220394 <2220394@ecc.ac.jp>
2023-07-21 11:13:52 +0900

    add:controllerの追加

---
+++ lib/Controllers/detail_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+import '../Views/detail_page.dart';
+
+class DetailController  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return DetailPage();
+  }
+}
