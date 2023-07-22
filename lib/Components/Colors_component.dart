c7529bcc3121dfac6e6b4f6e42384481b93684fe
2220394 <2220394@ecc.ac.jp>
2023-07-22 14:47:12 +0900

    add:色追加

--- lib/Components/Colors_component.dart
+++ lib/Components/Colors_component.dart
@@ -4,5 +4,5 @@ class Colors_component {
   static const globalBackgroundColor = Color(0xFFEAEAEA);
   static const titleColor = Color(0xFF928E65);
   static const gold = Color(0xFF9B8124);
-
+  static const fontgrey = Color(0xFF72777A);
 }
