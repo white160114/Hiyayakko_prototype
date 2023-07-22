a912f6e28c87999bdba1857c3993021acc880dbd
2220394 <2220394@ecc.ac.jp>
2023-07-22 17:50:13 +0900

    fix:controllerから値を渡すように

--- lib/Controllers/detail_controller.dart
+++ lib/Controllers/detail_controller.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hiyayakko/Arguments/food_to_detail.dart';
 
 import '../Views/detail_page.dart';
 
 class DetailController  extends StatelessWidget{
+  const DetailController({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return DetailPage();
+    final args = ModalRoute.of(context)!.settings.arguments as FoodToDetail;
+    String? name = args.name;
+    String? category = args.category;
+    String? expiryDate = args.expiryDate;
+    String? quantity = args.quantity;
+    String? imagePath = args.imagePath;
+
+    return DetailPage(
+      name: name,
+      category: category,
+      expiryDate: expiryDate,
+      quantity: quantity,
+      imagePath: imagePath,
+    );
   }
 }
--- lib/Controllers/editpage_controller.dart
+++ lib/Controllers/editpage_controller.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hiyayakko/Arguments/detail_to_edit.dart';
 
 import '../Views/edit_page.dart';
 
 class EditPageController  extends StatelessWidget{
+  const EditPageController({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return EditPage();
+    final args = ModalRoute.of(context)!.settings.arguments as DetailToEdit;
+    String? name = args.name;
+    String? category = args.category;
+    String? expiryDate = args.expiryDate;
+    String? quantity = args.quantity;
+    String? imagePath = args.imagePath;
+
+    return EditPage(
+      name: name,
+      category: category,
+      expiryDate: expiryDate,
+      quantity: quantity,
+      imagePath: imagePath,
+    );
   }
 }
