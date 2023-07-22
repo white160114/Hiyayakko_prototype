4ae874a0f1cd2f4bcc7abf52d3bf0e2bfb772bbc
2220394 <2220394@ecc.ac.jp>
2023-07-22 17:46:32 +0900

    add:argmentsに追加

---
+++ lib/Arguments/detail_to_edit.dart
@@ -0,0 +1,9 @@
+class DetailToEdit {
+  final String? name;
+  final String? category;
+  final String? expiryDate;
+  final String? quantity;
+  final String? imagePath;
+
+  DetailToEdit(this.name, this.category,this.expiryDate,this.quantity,this.imagePath);
+}
---
+++ lib/Arguments/food_to_detail.dart
@@ -0,0 +1,9 @@
+class FoodToDetail {
+  final String? name;
+  final String? category;
+  final String? expiryDate;
+  final String? quantity;
+  final String? imagePath;
+
+  FoodToDetail(this.name, this.category,this.expiryDate,this.quantity,this.imagePath);
+}
